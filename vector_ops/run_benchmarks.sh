1fde7ec1e9421e8a53d9678028c3ab7ed0b47afb
Maggie Liu <maggie.j.liu@gmail.com>
2025-10-06 02:44:45 -0400

    rename elementwise benchmarks

--- vector_ops/run_benchmarks.sh
+++ vector_ops/run_benchmarks.sh
@@ -3,8 +3,10 @@
 export OPENCILK_PATH=~/opencilk-project/build
 export KITSUNE_PATH=~/kitsune/build
 
-clang++ -O2 -o icicle_vector_add_benchmark icicle_vector_add_benchmark.cpp -I/home/magpie/zera-icicle-benchmarks/include -I/home/magpie/icicle-install/icicle/include -L/home/magpie/icicle-install/icicle/lib -licicle_device -licicle_field_bls12_381 -licicle_curve_bls12_381 -Wl,-rpath,/home/magpie/icicle-install/icicle/lib/
-python3 ../ock++.py -O2 -o zera_vector_add_benchmark zera_vector_add_benchmark.cpp -I/home/magpie/zera-icicle-benchmarks/include -I/home/magpie/icicle-install/icicle/include
+clang++ -O2 -o icicle_vector_elementwise_benchmark icicle_vector_elementwise_benchmark.cpp -I/home/magpie/zera-icicle-benchmarks/include -I/home/magpie/icicle-install/icicle/include -L/home/magpie/icicle-install/icicle/lib -licicle_device -licicle_field_bls12_381 -licicle_curve_bls12_381 -Wl,-rpath,/home/magpie/icicle-install/icicle/lib/
+python3 ../ock++.py -O2 -o zera_vector_elementwise_benchmark zera_vector_elementwise_benchmark.cpp -I/home/magpie/zera-icicle-benchmarks/include -I/home/magpie/icicle-install/icicle/include
 
-./icicle_vector_add_benchmark
-./zera_vector_add_benchmark
+./icicle_vector_elementwise_benchmark > output.txt
+./zera_vector_elementwise_benchmark >> output.txt
+
+python3 parse_results.py
